84c7044cb8d09982cf76ccca1e6673a894bf92f9
Zeni <gustavozeni@alunos.utfpr.edu.br>
2023-10-21 03:55:17 -0300

    Late night fixes

--- trabalho_4/pdi.py
+++ trabalho_4/pdi.py
@@ -9,66 +9,41 @@ global label
 
 def label_blobs(img):
 
-  """   
-  # grayscale
   img_gray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
   blob_list = []
-  """
-  h_img = len(img)
-  w_img = len(img[0])
+  w_img = img.shape[0] 
+  h_img = img.shape[1]
   label = 1
 
-  label_matrix = [ 
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,1,0,0,0,0,0,0,0],
-      [0,0,1,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,1,0,0,1,1,0,0],
-      [0,0,0,1,0,0,1,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,1,0,0,0,1,0,0,0],
-      [0,0,1,0,0,1,1,0,0,0],
-      [0,0,1,0,0,0,0,0,0,0]
-      ]
-  
-  for i in range(h_img):
-      for j in range(w_img):
-        if img[i][j] == 1:
-          label_matrix[i][j] = -1
-        else:
-          label_matrix[i][j] = 0
-
-  print("Label matrix: \n")
-  for i in range(h_img):
-      for j in range(w_img):
-          print(label_matrix[i][j], end=" ")
-      print()
-
-
   ### Retorna um blob_list com cada blob encontrada
-  def flood_fill(x, y):
+  def flood_fill(x, y, no_pixels):
     if (x<0 or x>=h_img or y<0 or y>=w_img):
-      return False
-    if img[x][y] != NOT_VISITED:
       return
-    if img[x][y] == VISITED:
+    if img_gray[x][y] != NOT_VISITED:
       return
-    if img[x][y] == NOT_VISITED:
-      label_matrix[x][y] = label
+    if img_gray[x][y] == VISITED:
+      return
+    if img_gray[x][y] == NOT_VISITED:
+      img_gray[x][y] = VISITED
+      no_pixels+=1
+      
 
     flood_fill(x-1,y)
     flood_fill(x+1,y)
     flood_fill(x,y-1)
     flood_fill(x,y+1)
-    # flood_fill(x-1,y-1)
-    # flood_fill(x-1,y+1)
-    # flood_fill(x+1,y-1)
-    # flood_fill(x+1,y+1)
+    flood_fill(x-1,y-1)
+    flood_fill(x-1,y+1)
+    flood_fill(x+1,y-1)
+    flood_fill(x+1,y+1)
 
   for y in range (0, h_img):
     for x in (0, w_img):
-      flood_fill(x, y)
-
+      if img[x][y] == 1
+        no_pixels = 1
+        flood_fill(x, y, no_pixels)
+        blob_list.add({size: no_pixels, label: label, dp: 0})
+        label+=1
       
     
   return label_matrix
